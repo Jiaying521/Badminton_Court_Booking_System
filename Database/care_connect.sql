bb1922f8a06f0f11048285bc75a1de1055709401
ZhenXin <Chin.Zhen.Xin@student.mmu.edu.my>
2026-03-31 22:17:16 +0800

    Database update

--- Database/care_connect.sql
+++ Database/care_connect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2026 at 06:21 AM
+-- Generation Time: Mar 31, 2026 at 04:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -31,16 +31,18 @@ CREATE TABLE `admins` (
   `id` int(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiry` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`id`, `username`, `email`, `password`) VALUES
-(0, 'heybi', 'chinzx1814@gmail.com', 'abc123'),
-(1, 'admin', '-', 'abc123');
+INSERT INTO `admins` (`id`, `username`, `email`, `password`, `reset_token`, `token_expiry`) VALUES
+(0, 'heybi', 'chinzx1814@gmail.com', '$2y$10$lKJR3lV5IxZSt0Wasb3dUuCFGooQPJd4EBDFSc0xb30V.N7quWA/e', NULL, NULL),
+(1, 'admin', '-', 'abc123', NULL, NULL);
 
 --
 -- Indexes for dumped tables
