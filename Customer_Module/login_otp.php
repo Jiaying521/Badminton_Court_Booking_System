a9f84e45ef69e46d0567c5d5966780dda7474440
Jiaying521 <gan.jia.ying@student.mmu.edu.my>
2026-04-29 14:50:22 +0800

    login

--- Customer_Module/login_otp.php
+++ Customer_Module/login_otp.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once __DIR__ . '/../config.php';
 header('Content-Type: application/json');
 $data = json_decode(file_get_contents('php://input'), true);
 $email = $data['email'] ?? '';
