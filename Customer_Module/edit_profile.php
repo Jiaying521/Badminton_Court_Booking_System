d16550300dab2ef4931c62d133690adb74060b06
heibi1217 <CHIN.ZHEN.XIN@student.mmu.edu.my>
2026-06-17 00:55:49 +0800

    fix customer profile

--- Customer_Module/edit_profile.php
+++ Customer_Module/edit_profile.php
@@ -5,6 +5,7 @@
 // ============================================================
 
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/functions.php';
 
 // Check if user is logged in, redirect to homepage if not
 if (!isLoggedIn()) redirect('homepage.php');
